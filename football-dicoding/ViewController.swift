553c06993bf0a3da76b2af5a3ca6d07fdabbf14a
Avendi <avendi@tebengan.id>
2020-04-20 11:19:58 +0700

    initial commit

---
+++ football-dicoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  football-dicoding
+//
+//  Created by Avendi Sianipar on 20/4/20.
+//  Copyright © 2020 Avendi Sianipar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
