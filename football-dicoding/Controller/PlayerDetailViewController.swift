e07b1d5dee1931148fec78201a29918023253c40
Avendi <avendi@tebengan.id>
2020-04-20 11:56:47 +0700

    Add profile cell and controller

---
+++ football-dicoding/Controller/PlayerDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerDetailViewController.swift
+//  football-dicoding
+//
+//  Created by Avendi Sianipar on 20/4/20.
+//  Copyright © 2020 Avendi Sianipar. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ football-dicoding/Controller/TopPlayerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TopPlayerViewController.swift
+//  football-dicoding
+//
+//  Created by Avendi Sianipar on 20/4/20.
+//  Copyright © 2020 Avendi Sianipar. All rights reserved.
+//
+
+import UIKit
+
+class TopPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
